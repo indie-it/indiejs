1f0b1dce7139b27646fda59d87dce09aee79e5c3
indie-it <indieitblog@gmail.com>
2017-05-16 17:44:26 +0200

    - ajout de déclencheurs sur insertion/màj de la collection Companies - petit bugfix sur le tabular qui affiche les actions (cas où pas d'objet option)

---
+++ server/hooks/companyHooks.js
@@ -0,0 +1,54 @@
+﻿Companies.after.insert(function (userId, doc) {
+
+	console.log("Companies.after.insert");
+
+	var companyNameAndField = doc.name;
+	if (doc.field) {
+		companyNameAndField += ` (${doc.field})`;
+	}
+
+	var action = {
+		actionType: Lists.actions.map.profileRecruiterCreate,
+		userid: userId,
+		options: {
+			company: companyNameAndField,
+			companyid: doc._id,
+			username: Meteor.user().username
+		}
+	};
+
+	Actions.insert(action, function (err, objId) {
+		if (err) {
+			console.error(err);
+		}
+		console.log("Action enregistrée");
+	});
+
+});
+Companies.after.update(function (userId, doc, fieldNames, modifier, options) {
+
+	console.log("Companies.after.update");
+
+	var companyNameAndField = doc.name;
+	if (doc.field) {
+		companyNameAndField += ` (${doc.field})`;
+	}
+
+	var action = {
+		actionType: Lists.actions.map.profileRecruiterUpdate,
+		userid: userId,
+		options: {
+			company: companyNameAndField,
+			companyid: doc._id,
+			username: Meteor.user().username
+		}
+	};
+
+	Actions.insert(action, function (err, objId) {
+		if (err) {
+			console.error(err);
+		}
+		console.log("Action enregistrée");
+	});
+
+});
--- server/methods/companyMethods.js
+++ server/methods/companyMethods.js
@@ -18,25 +18,7 @@ Meteor.methods({
 				throw new Meteor.Error(500, err.message);
 			}
 
-			var companyNameAndField = insertdoc.name;
-			if (insertdoc.field) {
-				companyNameAndField += ` (${insertdoc.field})`;
-			}
-
-			Actions.insert({
-				actionType: Lists.actions.map.profileRecruiterCreate,
-				userid: Meteor.userId(),
-				options: {
-					company: companyNameAndField,
-					companyid: objectid,
-					username: Meteor.user().username
-				}
-			}, function (err, objId) {
-				if (err) {
-					console.error(err);
-				}
-				console.log("Action enregistrée");
-			});
+			console.log("Insert successful!");
 		});
 	},
 	"company.update": function (docid, updatedoc) {
@@ -48,26 +30,6 @@ Meteor.methods({
 			if (err) {
 				throw new Meteor.Error(500, err.message);
 			}
-
-			var companyNameAndField = updatedoc.$set.name;
-			if (updatedoc.$set.field) {
-				companyNameAndField += ` (${updatedoc.$set.field})`;
-			}
-
-			Actions.insert({
-				actionType: Lists.actions.map.profileRecruiterUpdate,
-				userid: Meteor.userId(),
-				options: {
-					company: companyNameAndField,
-					companyid: docid,
-					username: Meteor.user().username
-				}
-			}, function (err, objId) {
-				if (err) {
-					console.error(err);
-				}
-				console.log("Action enregistrée");
-			});
 
 			console.log("Update successful!");
 
