ec0c500809a537b86146fedcd3d3e1d32b2d64b7
indie-it <indieitblog@gmail.com>
2017-06-07 11:49:32 +0200

    - nouvelle fct: recherche de missions - paquet node ajouté "escape-string-regexp"

---
+++ client/templates/missions/missionSearch.js
@@ -0,0 +1,198 @@
+﻿Template.missionSearch.onCreated(() => {
+	console.log('Template.missionSearch.onCreated()');
+
+	let template = Template.instance();
+
+	template.searchQuery = new ReactiveVar({});
+	template.searching = new ReactiveVar(false);
+
+	template.autorun(() => {
+		console.log("template.autorun");
+		template.subscribe('mission.search', template.searchQuery.get(), () => {
+			console.log("template.searchQuery.get() changed");
+
+			setTimeout(() => {
+
+				console.log("setTimeout ! searching = false");
+				template.searching.set(false);
+			}, 300);
+		});
+	});
+
+});
+
+Template.missionSearch.helpers({
+	searching() {
+		console.log('Template.missionSearch.helpers searching()');
+
+		return Template.instance().searching.get();
+	},
+	query() {
+		console.log('Template.missionSearch.helpers query()');
+
+		return Template.instance().searchQuery.get();
+	},
+	missions() {
+		console.log('Template.missionSearch.helpers missions()');
+
+		let missions = Missions.find({}, { sort: { updated: -1 } });
+		if (missions) {
+			return missions;
+		}
+	},
+	getCategories() {
+		if (!this.categories) {
+			return "";
+		}
+		return this.categories.join(", ");
+	},
+});
+
+Template.missionSearch.events({
+
+	'keyup [name="search-name"]'(event, template) {
+		console.log('keyup [name="search-name"]');
+
+		let value = event.target.value.trim();
+		let obj = template.searchQuery.get();
+
+		if (value !== '' && event.keyCode === 13) {
+			obj.name = value;
+			template.searchQuery.set(obj);
+			template.searching.set(true);
+		}
+
+		if (value === '') {
+			obj.name = value;
+			template.searchQuery.set(obj);
+		}
+		console.log(obj);
+	},
+
+	'keyup [name="search-description"]'(event, template) {
+		console.log('keyup [name="search-description"]');
+
+		let value = event.target.value.trim();
+		let obj = template.searchQuery.get();
+
+		if (value !== '' && event.keyCode === 13) {
+			obj.description = value;
+			template.searchQuery.set(obj);
+			template.searching.set(true);
+		}
+
+		if (value === '') {
+			obj.description = value;
+			template.searchQuery.set(obj);
+		}
+
+		console.log(obj);
+	},
+
+	'keyup [name="search-techs"]'(event, template) {
+		console.log('keyup [name="search-techs"]');
+
+		let value = event.target.value.trim();
+		let arrvalue = event.target.value.trim().split(",");
+		console.log(value);
+		let obj = template.searchQuery.get();
+
+		if (arrvalue !== '' && event.keyCode === 13) {
+			obj.technos = arrvalue;
+			template.searchQuery.set(obj);
+			template.searching.set(true);
+		}
+
+		if (value === '') {
+			obj.technos = [value];
+			template.searchQuery.set(obj);
+		}
+
+		console.log(obj);
+	},
+
+	'keyup [name="search-tjm"]'(event, template) {
+		console.log('keyup [name="search-tjm"]');
+
+		let value = event.target.value;
+
+		let obj = template.searchQuery.get();
+
+		if (value > 0 && event.keyCode === 13) {
+			obj.tjm = value;
+			template.searchQuery.set(obj);
+			template.searching.set(true);
+		}
+
+		if (value === '') {
+			obj.tjm = -1;
+			template.searchQuery.set(obj);
+		}
+
+		console.log(obj);
+	},
+
+	'keyup [name="search-city"]'(event, template) {
+		console.log('keyup [name="search-city"]');
+
+		let value = event.target.value.trim();
+		let obj = template.searchQuery.get();
+
+		if (value !== '' && event.keyCode === 13) {
+			obj.location = value;
+			template.searchQuery.set(obj);
+			template.searching.set(true);
+		}
+
+		if (value === '') {
+			obj.location = value;
+			template.searchQuery.set(obj);
+		}
+
+		console.log(obj);
+	},
+
+	'keyup [name="search-category"]'(event, template) {
+		console.log('keyup [name="search-category"]');
+
+		let value = event.target.value.trim();
+		let obj = template.searchQuery.get();
+
+		if (value !== '' && event.keyCode === 13) {
+			obj.category = value;
+			template.searchQuery.set(obj);
+			template.searching.set(true);
+		}
+
+		if (value === '') {
+			obj.category = value;
+			template.searchQuery.set(obj);
+		}
+
+		console.log(obj);
+	},
+
+	'keyup [name="search-start"]'(event, template) {
+		console.log('keyup [name="search-start"]');
+
+		let value = event.target.value.trim();
+		let obj = template.searchQuery.get();
+		var reg = /^\d{4}-\d{2}-\d{2}$/i;
+		var ok = reg.test(value);
+
+		if (value !== '' && event.keyCode === 13) {
+			if (ok === false) {
+				sAlert.error("Mauvais format de date. Utiliser le format suivant: 'aaaa-mm-jj'. Ex: 2016-04-01 pour le 01/04/2016)");
+			} else {
+
+				obj.start = value;
+				template.searchQuery.set(obj);
+				template.searching.set(true);
+			}
+		}
+		if (value === '') {
+			obj.start = value;
+			template.searchQuery.set(obj);
+		}
+	},
+});
--- server/publish/missionPubs.js
+++ server/publish/missionPubs.js
@@ -1,5 +1,7 @@
 ﻿import SimpleSchema from 'simpl-schema';
 import { check } from 'meteor/check';
+import { Match } from 'meteor/check';
+import escapeStringRegexp from 'escape-string-regexp';
 
 Meteor.publish('missions.headers', function () {
 	console.log("missions.headers");
@@ -31,4 +33,50 @@ Meteor.publish("mission.getById", function (missionId) {
 	check(missionId, String);
 
 	return Missions.find({ _id: missionId });
+});
+
+Meteor.publish('mission.search', function (search) {
+
+	check(search, Match.OneOf(Object, null, undefined));
+
+	//uniquement l'étape "validé"
+	let query = { $and: [{ "currentState.step": Lists.missionWorkflow.map.STEP_VALIDATED }] };
+	let projection = { limit: 10, sort: { updated: -1, createdAt: -1 } };
+
+	if (search) {
+
+		if (search.name) {
+			query.$and.push({ 'name': new RegExp(escapeStringRegexp(search.name), 'i') });
+		}
+		if (search.description) {
+			query.$and.push({
+				$or: [
+					{ 'description': new RegExp(escapeStringRegexp(search.description), 'i') },
+					{ 'shortDescription': new RegExp(escapeStringRegexp(search.description), 'i') },
+				]
+			});
+		}
+		if (search.technos && search.technos.length > 0 && search.technos[0] !== '') {
+			_.each(search.technos, (item) => {
+				if (item) query.$and.push({ 'technos': new RegExp(escapeStringRegexp(item.trim()), 'i') });
+			});
+		}
+		if (search.tjm && search.tjm > 0) {
+			query.$and.push({ 'averageDailyRate.min': { $gte: parseInt(search.tjm) } });
+		}
+		if (search.location) {
+			query.$and.push({ 'location': new RegExp(escapeStringRegexp(search.location), 'i') });
+		}
+		if (search.category) {
+			query.$and.push({ 'categories': new RegExp(escapeStringRegexp(search.category), 'i') });
+		}
+		if (search.start) {
+			query.$and.push({ 'start': { $gte: new Date(search.start) } });
+		}
+
+		//console.log(query);
+		projection.limit = 100;
+	}
+
+	return Missions.find(query, projection);
 });
